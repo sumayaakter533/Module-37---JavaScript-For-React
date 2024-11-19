9e5b02a564cd01eb4f6b7929a9708731d8538f4a
sumayaakter533 <sumaya55230@gmail.com>
2024-11-19 11:51:36 +0600

    [37.2] #ADD - ES6 Features (1.0) Feature 1 - Template string

---
+++ Topic-2--- ES6-Features/template-string.js
@@ -0,0 +1,10 @@
+const numbers = [1, 2, 3, 4, 5, 6, 7];
+const student = {
+    name: "Sumaya Akter",
+    age: 23,
+    subjects: ["Peripheral", "Operating system"],
+};
+
+const aboutMyslef = `My name is ${student.name} age of ${student.age}. I have ${numbers[1]} favorite subject. They are - ${student.subjects}`;
+
+console.log(aboutMyslef);
