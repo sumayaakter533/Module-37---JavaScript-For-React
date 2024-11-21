c7f2727927acb1c18b3506407078edea4678cef6
sumayaakter533 <sumaya55230@gmail.com>
2024-11-21 12:19:21 +0600

    [37.2] #UPDATE - ES6 Features (3.1) Feature 3 - Spread operator TODO - another example of spread operator is shown.

--- Topic-2--- ES6-Features/spread-operator.js
+++ Topic-2--- ES6-Features/spread-operator.js
@@ -6,3 +6,23 @@ newNumbers.push(99);
 
 console.log(numbers);
 console.log(newNumbers);
+
+const products = [
+    { name: "laptop", price: 3200, brand: "lenovo", color: "blue" },
+    { name: "phone", price: 1500, brand: "samsung", color: "black" },
+    { name: "tablet", price: 2000, brand: "apple", color: "silver" },
+    { name: "monitor", price: 800, brand: "dell", color: "white" },
+    { name: "keyboard", price: 50, brand: "logitech", color: "black" },
+    { name: "mouse", price: 20, brand: "hp", color: "red" },
+    { name: "headphones", price: 100, brand: "sony", color: "blue" },
+];
+
+const newProduct = {
+    name: "webcam",
+    price: 800,
+    brand: "Logitech",
+    color: "black",
+};
+
+const newProducts = [...products, newProduct];
+console.log(newProducts);
